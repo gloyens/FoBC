17264a044087492bf53e9af7943cd72685f3057a
George Gloyens <georgegloyens@gmail.com>
2022-12-30 15:01:22 +0000

    gallery tabs

--- js/main.js
+++ js/main.js
@@ -1,8 +1,26 @@
-function openNavbar() {
-  var x = document.getElementById("myNavbar");
-  if (x.className === "navbar") {
-    x.className += " responsive";
-  } else {
-    x.className = "navbar";
+// function openNavbar() {
+//   let x = document.getElementById("myNavbar");
+//   if (x.className === "navbar") {
+//     x.className += " responsive";
+//   } else {
+//     x.className = "navbar";
+//   }
+// }
+
+function openGallery(event, tagName) {
+  let i, x, tablinks;
+  x = document.getElementsByClassName("gallery");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
   }
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < x.length; i++) {    if (tablinks[i].classList.contains("tab-active")) {
+      tablinks[i].classList.toggle("tab-active");
+      tablinks[i].classList.toggle("tab");
+    } else if (tablinks[i] === event.currentTarget) {
+      tablinks[i].classList.toggle("tab-active");
+      tablinks[i].classList.toggle("tab");
+    }
+  }
+  document.getElementById(tagName).style.display = "flex";
 }
